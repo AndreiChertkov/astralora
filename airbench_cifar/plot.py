df36c64360b02d5edc3a25fe543f9667193589c2
AndreiChertkov <andre.chertkov@gmail.com>
2025-06-30 16:25:35 +0300

    Update scripts for plots

--- airbench_cifar/plot.py
+++ airbench_cifar/plot.py
@@ -2,10 +2,10 @@ import matplotlib as mpl
 import numpy as np
 import os
 
+
 mpl.rcParams.update({
     'font.size': 30,
-    'text.usetex': False,
-})
+    'text.usetex': False})
 
 
 import matplotlib.pyplot as plt
@@ -15,6 +15,9 @@ seeds = [1, 2, 3, 4, 5]
 ranks = [1, 3, 5, 7, 10, 50, 100]
 samples_list = [1, 10, 100, 1000][::-1]
 base_dir = "result" 
+Y_TICKS = [
+    [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
+    [50, '', 60, '', 70, '', 80, '', 90, '', 100]]
 
 
 def load():
@@ -22,18 +25,19 @@ def load():
     results['digital'] = []
     results['baseline'] = []
 
-    exp_name = f"digital"
-    acc = load_one(exp_name)
-    for rank in [0]+ranks:
-        results['digital'].append(acc)
+    exp_name = "digital"
+    digital_accs = load_one(exp_name)
+    results['digital'] = digital_accs
 
     for rank in ranks:
+        exp_name = f"bb_matvec_rank{rank}_baseline"
+        accs = load_one(exp_name)
+        results['baseline'].append(accs)
+
         for samples_val in samples_list:
             exp_name = f"bb_matvec_rank{rank}_samples{samples_val}"
-            results[samples_val].append(load_one(exp_name))
-        
-        exp_name = f"bb_matvec_rank{rank}_baseline"
-        results['baseline'].append(load_one(exp_name))
+            accs = load_one(exp_name)
+            results[samples_val].append(accs)
 
     return results
 
@@ -44,65 +48,54 @@ def load_one(name):
         fpath = os.path.join(base_dir, name + f'_seed{seed}', "result.npz")
         data = np.load(fpath, allow_pickle=True)
         res = data['res'].item()
-        accs.append(res['accs_tst'][-1])
-    return np.mean(accs)
+        accs.append(res['accs_tst'][-1] * 100)
+    return accs
+
+
+def plot_line(x, y, label, linestyle='-', linewidth=4,
+              marker='*', markersize=12, alpha=0.3):
+    x = np.array(x)
+    y = np.array(y)
+    y_avg = np.mean(y, axis=1)
+    dy = np.std(y, axis=1)
+
+    plt.plot(x, y_avg, linestyle, linewidth=linewidth, label=label,
+        marker=marker, markersize=markersize)
+    plt.fill_between(x, y_avg - dy, y_avg + dy, alpha=alpha)
 
 
 def plot():
     results = load()
-
+    
     fig, ax = plt.subplots(1, 1, figsize=(16, 12))
     plt.subplots_adjust(wspace=0.)
-
+    
     plt.xlabel("Rank")
     plt.ylabel("Accuracy, %")
-    plt.grid(True, linestyle='--', alpha=0.7)
+    
+    plot_line([0, max(ranks)], [results['digital'], results['digital']],
+        "Digital", linestyle='--', linewidth=7, marker=None)
 
-    plt.plot([0]+ranks, results['digital'], '--', linewidth=6, label=f"Digital")
-
-    valid_ranks = []
-    valid_accs = []
-
-    for i, rank in enumerate(ranks):
-        acc = results['baseline'][i]
-        if acc is not None:
-            valid_ranks.append(rank)
-            valid_accs.append(acc)
-
-    plt.plot(valid_ranks, valid_accs, '--', linewidth=6, marker='*', markersize=12,
-        label=f"Baseline")
+    plot_line(ranks, results['baseline'],
+        "Baseline", linestyle='--', linewidth=7, marker='o', markersize=16)
 
     for samples_val in samples_list:
-        valid_ranks = []
-        valid_accs = []
-        
-        for i, rank in enumerate(ranks):
-            acc = results[samples_val][i]
-            if acc is not None:
-                valid_ranks.append(rank)
-                valid_accs.append(acc)
-        
-        label = f"{samples_val} samples"
-        if samples_val == 1:
-            label = label[:-1]
-        plt.plot(valid_ranks, valid_accs, 'o-', linewidth=3, markersize=10,
-            label=label)
+        label = f"{samples_val} sample{'s' if samples_val > 1 else ''}"
+        plot_line(ranks, results[samples_val], label)
 
     plt.xscale('log')
-    # plt.yscale('log')
     plt.xticks(ranks, labels=[str(r) for r in ranks])
-
+    plt.yticks(*Y_TICKS)
     plt.legend(loc="best")
     plt.tight_layout()
 
-    ax.grid(ls=":")
+    ax.grid(True, linestyle='--', alpha=0.7)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
 
-    fpath = base_dir + "/result.png"
+    fpath = "result.png"
     plt.savefig(fpath, dpi=300, bbox_inches='tight')
     plt.close(fig)
     print('Saved plot in:', fpath)
