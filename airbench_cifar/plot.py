a7b05728f78347ca9515112f2679ba6e1cf41599
AndreiChertkov <andre.chertkov@gmail.com>
2025-07-04 15:04:08 +0300

    Small fix for plot generation

--- airbench_cifar/plot.py
+++ airbench_cifar/plot.py
@@ -14,35 +14,34 @@ import matplotlib.pyplot as plt
 seeds = [1, 2, 3, 4, 5]
 ranks = [1, 3, 5, 7, 10, 50, 100]
 samples_list = [1, 10, 100, 1000][::-1]
-base_dir = "result" 
 Y_TICKS = [
-    [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
-    [50, '', 60, '', 70, '', 80, '', 90, '', 100]]
+    [45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
+    ['', 50, '', 60, '', 70, '', 80, '', 90, '', 100]]
 
 
-def load():
+def load(base_dir):
     results = {s: [] for s in samples_list}
     results['digital'] = []
     results['baseline'] = []
 
     exp_name = "digital"
-    digital_accs = load_one(exp_name)
+    digital_accs = load_one(base_dir, exp_name)
     results['digital'] = digital_accs
 
     for rank in ranks:
         exp_name = f"bb_matvec_rank{rank}_baseline"
-        accs = load_one(exp_name)
+        accs = load_one(base_dir, exp_name)
         results['baseline'].append(accs)
 
         for samples_val in samples_list:
             exp_name = f"bb_matvec_rank{rank}_samples{samples_val}"
-            accs = load_one(exp_name)
+            accs = load_one(base_dir, exp_name)
             results[samples_val].append(accs)
 
     return results
 
 
-def load_one(name):
+def load_one(base_dir, name):
     accs = []
     for seed in seeds:
         fpath = os.path.join(base_dir, name + f'_seed{seed}', "result.npz")
@@ -64,8 +63,8 @@ def plot_line(x, y, label, linestyle='-', linewidth=4,
     plt.fill_between(x, y_avg - dy, y_avg + dy, alpha=alpha)
 
 
-def plot():
-    results = load()
+def plot(base_dir="result", fpath_plot="result.png"):
+    results = load(base_dir)
     
     fig, ax = plt.subplots(1, 1, figsize=(16, 12))
     plt.subplots_adjust(wspace=0.)
@@ -86,7 +85,7 @@ def plot():
     plt.xscale('log')
     plt.xticks(ranks, labels=[str(r) for r in ranks])
     plt.yticks(*Y_TICKS)
-    plt.legend(loc="best")
+    plt.legend(loc="lower right")
     plt.tight_layout()
 
     ax.grid(True, linestyle='--', alpha=0.7)
@@ -95,11 +94,14 @@ def plot():
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
 
-    fpath = "result.png"
-    plt.savefig(fpath, dpi=300, bbox_inches='tight')
+    plt.savefig(fpath_plot, dpi=300, bbox_inches='tight')
     plt.close(fig)
-    print('Saved plot in:', fpath)
+    print('Saved plot in:', fpath_plot)
 
 
 if __name__ == '__main__':
-    plot()
+    plot(base_dir="result",
+        fpath_plot="airbench_cifar_result.png")
+    
+    plot(base_dir="result_fixed", 
+        fpath_plot="airbench_cifar_result_samples_bb_1000.png")
